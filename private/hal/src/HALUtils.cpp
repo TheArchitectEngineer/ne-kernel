f36b63c3de27e7e0ffb7de78417466a515184956
Amlal El Mahrouss <amlal@nekernel.org>
2026-09-17 18:21:20 +0200

    UPDATE: add HALUtils.cpp for entries support

---
+++ private/hal/src/HALUtils.cpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
+// Official repository: https://github.com/ne-app-eu/krnl
+
+/// For Entries Support.
+#include <ArchKit/ArchKit.h>
+#include <hal/HAL/HAL.h>
