3ab946b27076d6d40c19e3661d07337f5d354da2
Amlal El Mahrouss <amlal@nekernel.org>
2026-09-23 05:46:09 +0200

    CHORE: update hardware timer macro for semaphore wait function

--- private/minkernel/KernelKit/Semaphore.h
+++ private/minkernel/KernelKit/Semaphore.h
@@ -94,7 +94,7 @@ inline Bool rtl_sem_wait(SemaphoreArr& sem, const UInt64& owner, const UInt64& t
     return true;
   }
 
-#ifdef __ANT_USE_HW_TIMER__
+#ifdef __NE_USE_HW_TIMER__
   HardwareTimer timer(timeout);
 #else
   SoftwareTimer timer(timeout);
